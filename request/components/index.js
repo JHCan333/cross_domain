296e8bb6ec409ebe6fa41fe8cad5745e59c2f17f
JHCan333 <15703270801@163.com>
2019-09-20 17:39:33 +0800

    添加readme

--- request/components/index.js
+++ request/components/index.js
@@ -1,3 +1,4 @@
+
 import ByCORS from './ByCORS.vue'
 import Normal from './Normal.vue'
 import ByJsonp from './ByJsonp.vue'
--- request/index.js
+++ request/index.js
@@ -1,5 +1,4 @@
-
-// import Vue from 'vue'
+// 项目主目录
 import Vue from 'vue/dist/vue.esm.js'
 import App from './App.vue'
 import axios from 'axios'
